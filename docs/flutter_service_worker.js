75218092a0c9a925278e5860cc1b7764281628a1
Lam <lamtk.84@gmail.com>
2021-09-29 20:25:32 +0700

    deploy web again

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -25,8 +25,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "ee57afe7bb8e4a88680c9bcf5353f6c9",
-"/": "ee57afe7bb8e4a88680c9bcf5353f6c9",
+"index.html": "ba58dba77a02faecaf562f45fd651a30",
+"/": "ba58dba77a02faecaf562f45fd651a30",
 "main.dart.js": "434d8f033d3d8da59b93c84eff63f742",
 "manifest.json": "76b30d5b1a43c91f0d40417159c2f376",
 "version.json": "9d64ec68f6ea6fc47daf793b1f175ea0"
