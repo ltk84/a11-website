f2731ca8fc8a70ba9104da47455bbea55de59586
TungTLT <tranlethanhtung554@gmail.com>
2021-10-03 18:41:06 +0700

    init some demo code of viewModel

--- lib/core/services/cloud_firestore/cloud_firestore_service_implementation.dart
+++ lib/core/services/cloud_firestore/cloud_firestore_service_implementation.dart
@@ -1,3 +1,8 @@
 import 'package:a11_website/core/services/cloud_firestore/cloud_firestore_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class FirestoreServiceImpl extends FirestoreService {}
+class FirestoreServiceImpl extends FirestoreService {
+  FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  getData() {}
+}
---
+++ lib/core/view_models/albums_view_model.dart
@@ -0,0 +1,13 @@
+import 'package:a11_website/core/models/album.dart';
+import 'package:a11_website/core/services/cloud_firestore/cloud_firestore_service_implementation.dart';
+import 'package:flutter/cupertino.dart';
+
+class AlbumsViewModel extends ChangeNotifier {
+  FirestoreServiceImpl _firestoreService = FirestoreServiceImpl();
+  List<Album> albums = [];
+
+  void loadAlbums() async {
+    albums = await _firestoreService.getData();
+    notifyListeners();
+  }
+}
